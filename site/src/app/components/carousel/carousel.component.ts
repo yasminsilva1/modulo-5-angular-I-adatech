0b57ad944e220933ce28dbcebcb4c28b1bad171c
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-10-30 20:57:35 -0300

    Aula 04 - pt2 - Event Binding

--- site/src/app/components/carousel/carousel.component.ts
+++ site/src/app/components/carousel/carousel.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent {
+  message?: string;
 
+  // * Event Binding
+  onButtonClicked(direction: string): void {
+    this.message = `Botão ${direction} acionado!`;
+  }
 }

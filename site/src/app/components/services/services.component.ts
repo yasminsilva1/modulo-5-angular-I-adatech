632d867660158d683100dd1de89eb3122814c150
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-10-30 20:58:36 -0300

    Aula 04 - pt4 - ngStyle e ngClass

--- site/src/app/components/services/services.component.ts
+++ site/src/app/components/services/services.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.css']
+  styleUrls: ['./services.component.css'],
 })
 export class ServicesComponent {
+  textColor = 'black';
+  grade = 6;
 
+  coolText = true;
+
+  changeGradeColor(): void {
+    this.textColor = this.grade >= 7 ? 'blue' : 'red';
+  }
+
+  isCoolText(): boolean {
+    return true;
+  }
 }

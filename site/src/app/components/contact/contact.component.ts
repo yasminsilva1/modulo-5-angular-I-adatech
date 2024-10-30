93ebdca5c5b027b620c4bc9f0101cf5943a16133
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-10-30 20:57:50 -0300

    Aula 04 - pt3 - Two Way Binding

--- site/src/app/components/contact/contact.component.ts
+++ site/src/app/components/contact/contact.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
+  contact: { email?: string; message?: string } = {
+    email: "fulano@email.com",
+    message: "Minha mensagem"
+  };
 
+  onSubmit(): void {
+    console.log(this.contact);
+  }
 }

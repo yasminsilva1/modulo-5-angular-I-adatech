8a633b8d753cdc7838ba6d46815b7f09f814e889
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2024-11-01 21:11:49 -0300

    Aula 05 - pt3 - Adiciona botão de incremento e exibe preço do produto
    - Adiciona propriedade `@Output` com `EventEmitter` para emitir evento de incremento.
    - Inclui método `increment()` no componente `CardComponent` para emitir o evento.
    - Atualiza template HTML para exibir o preço do produto (`{{product.price}}`).
    - Adiciona botão de incremento no rodapé do card para acionar o método `increment()`.
    
    Essas mudanças permitem que o card envie eventos de incremento e exiba informações adicionais sobre o produto.

--- loja/src/app/shared/components/card/card.component.ts
+++ loja/src/app/shared/components/card/card.component.ts
@@ -1,11 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
   @Input() product!: Product;
+  @Output() incrementEmitter = new EventEmitter<void>();
+
+  increment(): void {
+    this.incrementEmitter.emit();
+  }
 }
